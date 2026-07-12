2843452464cf59a9a3e8cc4bedcc62488ec08daf
Omar Farouk Mohammed Haider Al-Suhily <omardash25@gmail.com>
2026-07-12 21:27:00 +0300

    feat: add React app with route structure

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Jobs from './pages/Jobs';
+import JobDetail from './pages/JobDetail';
+import PostJob from './pages/PostJob';
+import Profile from './pages/Profile';
+import Messages from './pages/Messages';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="jobs" element={<Jobs />} />
+          <Route path="jobs/:id" element={<JobDetail />} />
+          <Route path="jobs/new" element={<PostJob />} />
+          <Route path="profile/:id" element={<Profile />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
