5c4d25b53589b8129358b8b24c088e013269dcfe
Omar Farouk Mohammed Haider Al-Suhily <omardash25@gmail.com>
2026-07-12 21:27:48 +0300

    feat: add page component stubs

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Find Top Freelancers for Your Project</h1>
+      <p>Connect with skilled professionals. Get work done.</p>
+    </div>
+  );
+}
