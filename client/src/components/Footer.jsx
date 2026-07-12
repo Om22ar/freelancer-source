b51817e1da6700584d4d26145bec718e3355052b
Omar Farouk Mohammed Haider Al-Suhily <omardash25@gmail.com>
2026-07-12 21:27:55 +0300

    feat: add Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-50 border-t py-8 mt-auto">
+      <div className="max-w-7xl mx-auto px-4 text-center text-gray-500">
+        <p>&copy; 2026 Freelancer Source. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
